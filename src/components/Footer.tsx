8a8befed323247936a6bcbe2583f01d127846da1
Michael Chung <michael061195@gmail.com>
2024-09-13 16:29:57 -0500

    home page styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
-
+import Link from "next/link";
+import path from "path";
+import React from "react";
+const routes = [
+  {
+    path: "/terms-conditions",
+    name: "Terms & Conditions",
+  },
+  {
+    path: "/privacy-policy",
+    name: "Privacy Policy",
+  },
+];
 export default function Footer() {
   return (
-    <footer>
-      <p>hi</p>
+    <footer
+      className="
+    mt-auto flex items-center justify-between h-16 border-t border-white/10 px-3 sm:px-9 text-xs text-white/25"
+    >
+      <small className="text-xs">&Copy; 2050 Mike. All rights pending</small>
+      <ul className="flex gap-x-3 sm:gap-x-8">
+        {routes.map((route) => (
+          <li key={route.path}>
+            <Link href={route.path}>{route.name}</Link>
+          </li>
+        ))}
+      </ul>
     </footer>
-  )
+  );
 }
