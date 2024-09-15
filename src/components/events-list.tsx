079275e23c4820ef332ba3c09e8a39ac0cfc8cc6
Michael Chung <michael061195@gmail.com>
2024-09-15 14:21:57 -0500

    event page

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -1,15 +1,15 @@
-import { EventoEvent } from '@/lib/types'
-import React from 'react'
-import EventCard from './event-card'
+import { EventoEvent } from "@/lib/types";
+import React from "react";
+import EventCard from "./event-card";
 type EventsListProps = {
-    events: EventoEvent[]
-}
-export default function EventsList({events}:EventsListProps) {
+  events: EventoEvent[];
+};
+export default function EventsList({ events }: EventsListProps) {
   return (
-    <section className='flex flex-wrap gap-10 justify-center px-[20px]'>
+    <section className="max-w-[1100px] flex flex-wrap gap-10 justify-center px-[20px]">
       {events.map((event) => (
-      <EventCard key={event.id} event={event}/>
+        <EventCard key={event.id} event={event} />
       ))}
     </section>
-  )
+  );
 }
