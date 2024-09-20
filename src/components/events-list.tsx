884b521133b039500055d0055b80894f1c575f8e
Michael Chung <michael061195@gmail.com>
2024-09-20 15:39:15 -0500

    zod authentication

--- src/components/events-list.tsx
+++ src/components/events-list.tsx
@@ -6,9 +6,9 @@ import PaginationControls from "./paginationControls";
 
 type EventsListProps = {
   city: string;
-  page: number;
+  page?: number;
 };
-export default async function EventsList({ city, page }: EventsListProps) {
+export default async function EventsList({ city, page = 1 }: EventsListProps) {
   const { events, totalCount } = await getEvents(city, page);
   const previousPath = page > 1 ? `/events/${city}?page=${page - 1}` : "";
   const nextPath =
